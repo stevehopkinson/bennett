a99bb44e7d4395e1871cc658454642958054dea0
Steve Hopkinson <steve@stevehopkinson.co.uk>
2017-06-21 17:11:05 +0100

    Rewrite User route

--- config/routes.php
+++ config/routes.php
@@ -3,10 +3,10 @@ use Cake\Core\Plugin;
 use Cake\Routing\Router;
 
 Router::scope('/', function ($routes) {
+    $routes->extensions(['json']);
+
     $routes->connect('/users', [
         'controller' => 'Users',
         'action' => 'index'
     ]);
-
-    $routes->extensions(['json']);
 });
